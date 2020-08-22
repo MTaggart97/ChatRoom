0eb23a0cb48dfc24c01742b640a353409b7e4742
Matthew Taggart <mtaggart123@gmail.com>
2020-08-22 21:31:15 +0100

    Server can handle multiple connections using threads. Clients maintained in a list on the server. Clients can message each other in the same groups

---
+++ ClientList.py
@@ -0,0 +1,102 @@
+from socket import socket
+
+
+class ClientList:
+    """
+    Class to represent the client list that the server maintains
+    """
+    client_list = []
+
+    def __init__(self):
+        self.client_list = []
+
+    def addToList(self, conn: socket, name: str, chat_room: str):
+        """
+        Adds a connection to the list with the inputted data
+
+        Parameters:
+            self (ClientList): This instance
+            conn (socket): Connection to client
+            name (str): Name of client
+            chat_room (str): Chat room that they are in
+
+        Returns:
+            (boolean): True if item was successfully added
+        """
+        temp_dict = {'Connection': conn, 'Name': name, 'Room': chat_room}
+        for entry in self.client_list:
+            if (entry['Connection'] == conn and entry['Name'] == name
+                    and entry['Room'] == chat_room):
+                return False
+        self.client_list.append(temp_dict)
+
+    def removeFromList(self, conn: socket):
+        """
+        Removes an item from the list give the connection
+
+        Parmeters:
+            self (ClientList): This instance
+            conn (socket): Connection to client
+        Returns:
+            (boolean): True if item was removed successfully
+        """
+        for entry in self.client_list:
+            if entry['Connection'] == conn:
+                self.client_list.remove(entry)
+                return True
+        return False
+
+    def getList(self):
+        """
+        Returns a copy of the list of clients
+        """
+        return self.client_list.copy()
+
+    def connectionsInRoom(self, chat_room: str):
+        """
+        Finds all clients in a give chat room.
+
+        Parameters:
+            self (ClientList): This instance
+            chat_room (str): Chat room to search for
+
+        Returns:
+            (list): A list of connections in that room
+        """
+        connections = []
+        for entry in self.client_list:
+            if entry['Room'] == chat_room:
+                connections.append(entry['Connection'])
+        return connections
+
+    def getConnRoom(self, conn: socket):
+        """
+        Gets the current room that this connection is in.
+
+        Parameters:
+            self (ClientList): This instance
+            conn (socket): Connections to search for
+
+        Returns:
+            (str): The current chat room of this connection
+        """
+        for entry in self.client_list:
+            if entry['Connection'] == conn:
+                return entry['Room']
+        return None
+
+    def getName(self, conn: socket):
+        """
+        Given a socket, return the name of that user.
+
+        Parameters:
+            self (ClientList): This instance
+            conn (socket): Connections to search for
+
+        Returns:
+            (str): The name of the user of that connection
+        """
+        for entry in self.client_list:
+            if entry['Connection'] == conn:
+                return entry['Name']
+        return None
