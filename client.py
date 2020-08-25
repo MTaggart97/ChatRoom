68deedfeae7b63281b0787ab0e1aa1c5ae458d31
Matthew Taggart <mtaggart123@gmail.com>
2020-08-25 19:49:27 +0100

    Create GUI with Tkinter
    This GUI fixes the bug where messages being typed would be obscured by incoming messages on the commandline. Now incoming messages and typed messages are in seperate widgets of the GUI so no chance of overlap

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
+from queue import Queue
 import socket
 import threading
+import tkinter as tk
+from tkinter import messagebox
 
 host = 'localhost'
 port = 5000
@@ -10,22 +13,76 @@ DISCONNECT = '/disconnect'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(addr)
 
+msgRcvQueue = Queue()   # FIFO Queue
+window = tk.Tk()        # Main window
+frame = tk.Frame()      # Frame containing history of messages
 
-def handle_send():
+def checkForMessages():
     """
-    This is run in it's own thread to send messages to the server.
-    It waits for user input, then sends the message on hitting the
-    Enter key. This is a blocking action which is why it is in it's
-    own thread.
+    Checks the msgRcvQueue for messages. If there is a message in the
+    queue, get it and create a label with the message in it. Message is
+    removed once it is used.
     """
-    connected = True
-    # While connected, read user input and send to the server
-    while connected:
-        userInput = input()
-        s.sendall(userInput.encode())
-        if userInput == DISCONNECT:
-            connected = False
-    s.close()
+    if not msgRcvQueue.empty():
+        label = tk.Label(text=msgRcvQueue.get(), master=frame)
+        label.pack()
+    window.after(100, checkForMessages)
+
+def setUpWindow():
+    """
+    Populates the window that the user will use to send and
+    recieve messages. Consists of a frame full of labels that is used to
+    show the messages. A text box to type messages and a send button.
+
+    Parameters:
+        window (tkinter): The window to display
+    """
+    input_box = tk.Text()
+    button = tk.Button(text="Send")
+    # Using a lambda here so that the inputBox can be passed in as
+    # and argument
+    button.bind("<Button-1>",
+                lambda event, inputBox=input_box:
+                    handle_send(inputBox))
+
+    frame.pack()
+    input_box.pack()
+    button.pack()
+
+    window.protocol("WM_DELETE_WINDOW", on_close)
+    window.after(0, checkForMessages)
+
+
+def handle_send(input_box: tk.Text):
+    """
+    Executed when the user presses send. It will get and clear the
+    text box then send the message. The message will be put straight
+    into the list of messages on the client side. This is done so that
+    the message doesn't need to be sent to the server and back again.
+
+    Parameter:
+        input_box (tk.Text): Text box containing the message
+    """
+    msg = input_box.get("1.0", tk.END).rstrip()
+    input_box.delete("1.0", tk.END)
+    s.sendall(msg.encode())
+    # Create a label with the clients message and add it to the frame
+    label = tk.Label(text='You: ' + msg, master=frame)
+    label.pack()
+    if msg == DISCONNECT:
+        s.close()
+        window.destroy()
+
+def on_close():
+    """
+    Funciton to handle when the user closes the window instead of typing
+    the DISCONNECT message. First notifies the server that you are
+    disconnecting, then closes the socket and window.
+    """
+    if messagebox.askokcancel("Quit", "Do you want to quit?"):
+        s.sendall(DISCONNECT.encode())
+        s.close()
+        window.destroy()
 
 
 def handle_recv():
@@ -38,7 +95,7 @@ def handle_recv():
     while True:
         try:
             data = s.recv(1024)
-            print(data.decode())
+            msgRcvQueue.put(data.decode())
         except ConnectionAbortedError:
             break
         except OSError:
@@ -46,10 +103,10 @@ def handle_recv():
 
 
 def main():
-    thread_send = threading.Thread(target=handle_send)
-    thread_send.start()
     thread_recv = threading.Thread(target=handle_recv)
     thread_recv.start()
+    setUpWindow()
+    window.mainloop()
 
 
 if __name__ == '__main__':
